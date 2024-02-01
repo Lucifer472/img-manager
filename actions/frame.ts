d6c5a661223aa68b5543a1986b71cf66569e4cbc
Lucifer472 <JanvI@0427>
2024-02-01 14:01:19 +0530

    finilized

--- actions/frame.ts
+++ actions/frame.ts
@@ -1,21 +1,10 @@
 "use server";
 
 import * as z from "zod";
-import mysql from "mysql";
+import mysql from "mysql2/promise";
 import { v4 as uuidv4 } from "uuid";
 
 import { frameSchema } from "@/schema";
-
-const connection = mysql.createConnection({
-  host: "main-db-do-user-15091144-0.c.db.ondigitalocean.com",
-  port: 25060,
-  user: "doadmin",
-  password: "AVNS__XXtZCugy3HFpWEWp7o",
-  database: "frames",
-  ssl: {
-    ca: process.env.DB_CA,
-  },
-});
 
 export const createFrames = async (v: z.infer<typeof frameSchema>) => {
   const validateFields = frameSchema.safeParse(v);
@@ -26,14 +15,22 @@ export const createFrames = async (v: z.infer<typeof frameSchema>) => {
 
   const id = uuidv4();
 
+  const connection = await mysql.createConnection({
+    host: "main-db-do-user-15091144-0.c.db.ondigitalocean.com",
+    port: 25060,
+    user: "doadmin",
+    password: "AVNS__XXtZCugy3HFpWEWp7o",
+    database: "frames",
+  });
+
   try {
     // Connect to the database
     await connection.connect();
 
     // Perform your database query
     const results = await connection.query(
-      `INSERT INTO frames.Frames (id, img, name, desc, supporter, userId) VALUES (?, ?, ?, ?, ?, ?)`,
-      [id, v.img, v.name, v.desc, "0", "clryj1bly000009l52u1kgneg"]
+      `INSERT INTO Frames ('id', 'img', 'name', 'desc', 'supporter', 'userId') VALUES (?, ?, ?, ?, ?, ?)`,
+      [id, v.img, v.name, v.desc, 0, "clryj1bly000009l52u1kgneg"]
     );
 
     // console.log("The solution is: ", results);
